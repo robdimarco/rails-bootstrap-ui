5d7c693781cc866dcb4e1b1a44fd5abf644d8633
Darian Shimy <dshimy@gmail.com>
2011-11-17 12:25:41 -0800

    Created an empty GEM project as:   $ bundle gem rails-bootstrap-ui

---
+++ lib/rails-bootstrap-ui.rb
@@ -0,0 +1,9 @@
+require "rails-bootstrap-ui/version"
+
+module Rails
+  module Bootstrap
+    module Ui
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails-bootstrap-ui/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Bootstrap
+    module Ui
+      VERSION = "0.0.1"
+    end
+  end
+end
